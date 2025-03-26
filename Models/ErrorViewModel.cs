09d7c91cfeb3eb2fbc5d76126c02ba63b65c02a2
JanJanssen_BFW <info@teckibase.de>
2025-03-26 13:33:38 +0100

    Projektdateien hinzufügen.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPnet_Jobtastic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
