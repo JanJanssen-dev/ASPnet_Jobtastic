15f61a1f6ab6d5f259baf0dd89e015c47abd1081
JanJanssen_BFW <info@teckibase.de>
2025-04-01 12:35:32 +0200

    Ersetze Identity durch EF und verbessere JobPosting-Logik
    Die `Microsoft.AspNetCore.Identity`-Bibliothek wurde entfernt und durch `Microsoft.EntityFrameworkCore` ersetzt. Der Konstruktor des `JobPostingController` wurde angepasst, um die Datenbankverbindung herzustellen. Die Methode `Index` wurde formatiert, um die Abfrage der JobPostings übersichtlicher zu gestalten. Die Methode `CreatedEditJobPosting` wurde in `CreateJob` umbenannt und die zugehörige View wurde angepasst. Eine neue `HttpPost`-Methode `CreateJob` wurde hinzugefügt, um neue JobPostings zu speichern. Diese Methode überprüft nun das `ModelState` und speichert das JobPosting nur, wenn es gültig ist. Die Methode `EditJob` wurde erweitert, um die Bearbeitung bestehender JobPostings zu ermöglichen. Dabei wird nun auch die Validierung des `ModelState` berücksichtigt und eine `DbUpdateConcurrencyException` behandelt. Die Methode `DeleteJob` wurde formatiert, um die Überprüfung auf `null` übersichtlicher zu gestalten. In der View `CreatedEditJobPosting.cshtml` wurde die Logik zur Anzeige des Titels und der Formularaktion basierend auf der `Id` des Modells hinzugefügt. Validierungsnachrichten wurden in der View `CreatedEditJobPosting.cshtml` hinzugefügt. Ein Button "Zurück zur Übersicht" wurde in der View `CreatedEditJobPosting.cshtml` hinzugefügt. Die `Index.cshtml`-View wurde angepasst, um den Link zur Erstellung eines neuen JobPostings zu aktualisieren. Ein Abschnitt für Validierungsskripte wurde in der View `CreatedEditJobPosting.cshtml` hinzugefügt.

--- Controllers/JobPostingController.cs
+++ Controllers/JobPostingController.cs
@@ -1,34 +1,39 @@
 ﻿using ASPnet_Jobtastic.Data;
 using ASPnet_Jobtastic.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASPnet_Jobtastic.Controllers
 {
     public class JobPostingController : Controller
     {
-        //DB verbindung herstellen im Controller
         private readonly ApplicationDbContext _context;
+
         public JobPostingController(ApplicationDbContext context)
         {
             _context = context;
         }
 
+        // Übersicht aller JobPostings des eingeloggten Users
         public IActionResult Index()
         {
-            var jobPostingsFromDb = _context.JobPostings.Where(x => x.OwnerUsername == User.Identity.Name).ToList();
+            var jobPostingsFromDb = _context.JobPostings
+                .Where(x => x.OwnerUsername == User.Identity.Name)
+                .ToList();
             return View(jobPostingsFromDb);
         }
 
-        public IActionResult CreatedEditJobPosting(int id)
+        // GET: Neues JobPosting anlegen
+        public IActionResult CreateJob()
         {
-            return View();
+            return View("CreatedEditJobPosting", new JobPostingModel());
         }
 
+        // POST: Neues JobPosting speichern
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult CreateJob(JobPostingModel jobPostingModel, IFormFile file)
         {
-
-            //Username Abfangen
             jobPostingModel.OwnerUsername = User.Identity.Name;
 
             if (file != null)
@@ -36,48 +41,86 @@ namespace ASPnet_Jobtastic.Controllers
                 using (var ms = new MemoryStream())
                 {
                     file.CopyTo(ms);
-                    var bytes = ms.ToArray();
-                    jobPostingModel.CompanyImage = bytes;
+                    jobPostingModel.CompanyImage = ms.ToArray();
                 }
             }
-            _context.JobPostings.Add(jobPostingModel);
-            _context.SaveChanges();   
-
-
-            return RedirectToAction("Index");
-        }
-
-        // GET: JobPosting/Edit/{ID}
-        public IActionResult EditJob(int id) 
-        {
-            var job = _context.JobPostings.Find(id);
-            if (job == null) return View("Error");
-            return View("CreatedEditJobPosting",job);
-        }
-
-        // POST: JobPostings/Edit/{ID}
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult EditJob(int id, JobPostingModel jobPostingModel)
-        {
-            if (id != jobPostingModel.Id) return BadRequest();
 
             if (ModelState.IsValid)
             {
-                _context.Update(jobPostingModel);
+                _context.JobPostings.Add(jobPostingModel);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
-            return View(jobPostingModel);
+
+            return View("CreatedEditJobPosting", jobPostingModel);
         }
 
-        // POST: JobPosting/Delete/{ID}
+        // GET: Bestehendes JobPosting bearbeiten
+        public IActionResult EditJob(int id)
+        {
+            var job = _context.JobPostings.Find(id);
+            if (job == null)
+                return NotFound();
+
+            return View("CreatedEditJobPosting", job);
+        }
+
+        // POST: Bearbeitetes JobPosting speichern
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditJob(int id, JobPostingModel jobPostingModel, IFormFile file)
+        {
+            if (id != jobPostingModel.Id)
+                return BadRequest();
+
+            var existingJob = _context.JobPostings.AsNoTracking().FirstOrDefault(j => j.Id == id);
+            if (existingJob == null)
+                return NotFound();
+
+            jobPostingModel.OwnerUsername = existingJob.OwnerUsername;
+
+            if (file != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    jobPostingModel.CompanyImage = ms.ToArray();
+                }
+            }
+            else
+            {
+                jobPostingModel.CompanyImage = existingJob.CompanyImage;
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jobPostingModel);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.JobPostings.Any(e => e.Id == jobPostingModel.Id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View("CreatedEditJobPosting", jobPostingModel);
+        }
+
+        // POST: JobPosting löschen
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult DeleteJob(int id)
         {
             var job = _context.JobPostings.Find(id);
-            if (job == null) return NotFound();
+            if (job == null)
+                return NotFound();
 
             _context.JobPostings.Remove(job);
             _context.SaveChanges();
