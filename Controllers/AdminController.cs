a124a75bf43e4e79dc39600b1c9d05dffefbc89d
JanJanssen_BFW <info@teckibase.de>
2025-04-03 11:35:21 +0200

    lauffähig

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPnet_Jobtastic.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
