4347dae92b2defa791ae2662624d03159c53dd52
JanJanssen_BFW <info@teckibase.de>
2025-04-14 13:04:32 +0200

    Neuen API-Controller für Jobangebote hinzugefügt
    Fügt einen API-Controller in `ApiJobPostingController.cs` hinzu, der Methoden zum Abrufen aller Jobangebote und eines spezifischen Jobangebots bereitstellt.
    
    Änderungen in `_Layout.cshtml` umfassen die Anpassung des HTML-Dokuments, die Integration eines Logo-Buttons in die Navigation und eine transparentere Fußzeile.
    
    In `site.css` wurden die Stile aktualisiert, um Transparenz in Header und Footer zu ermöglichen, sowie neue Stile für den Logo-Button und die Navbar-Links hinzugefügt, um die Sichtbarkeit zu verbessern.

---
+++ Controllers/ApiJobPostingController.cs
@@ -0,0 +1,40 @@
+﻿using ASPnet_Jobtastic.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+
+namespace ASPnet_Jobtastic.Controllers
+{
+    [Route("api/jobposting")]
+    [ApiController]
+
+    public class ApiJobPostingController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApiJobPostingController(ApplicationDbContext context)
+        {
+            
+            _context = context;
+        }
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var allJobPostings = _context.JobPostings.ToList();
+            return Ok(allJobPostings);
+        }
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+
+            var getJobPostingById = _context.JobPostings.SingleOrDefault(x => x.Id == id);
+
+            if (getJobPostingById == null)
+            {
+                return NotFound();
+            }
+            return Ok(getJobPostingById);
+        }
+    }
+}
