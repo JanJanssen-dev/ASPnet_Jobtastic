a17d635891adc371c3f73e5cc1772a68af8a3f48
JanJanssen_BFW <info@teckibase.de>
2025-04-15 10:53:35 +0200

    API für Jobangebote erweitert
    In der Datei `ApiJobPostingController.cs` wurden neue Routen und Methoden hinzugefügt, um Jobangebote zu erstellen, zu löschen und zu aktualisieren. Die neuen Endpunkte umfassen `GetById`, `Create`, `Delete` und `Update`. Validierungen wurden implementiert, um sicherzustellen, dass die ID bei der Erstellung eines neuen Jobangebots nicht gesetzt ist und dass die ID bei der Aktualisierung vorhanden ist.

--- Controllers/ApiJobPostingController.cs
+++ Controllers/ApiJobPostingController.cs
@@ -1,4 +1,6 @@
 ﻿using ASPnet_Jobtastic.Data;
+using ASPnet_Jobtastic.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +8,7 @@ using SQLitePCL;
 
 namespace ASPnet_Jobtastic.Controllers
 {
+    
     [Route("api/jobposting")]
     [ApiController]
 
@@ -24,6 +27,7 @@ namespace ASPnet_Jobtastic.Controllers
             var allJobPostings = _context.JobPostings.ToList();
             return Ok(allJobPostings);
         }
+
         [HttpGet("GetById")]
         public IActionResult GetById(int id)
         {
@@ -35,6 +39,45 @@ namespace ASPnet_Jobtastic.Controllers
                 return NotFound();
             }
             return Ok(getJobPostingById);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create(JobPostingModel jobPostingModel)
+        {
+            if (jobPostingModel.Id != 0)
+            {
+                return BadRequest("Error: Darf keine Id enthalten!");
+            }
+            _context.JobPostings.Add(jobPostingModel);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id)
+        {
+            var getJobpostingById = _context.JobPostings.Find(id);
+
+            if (getJobpostingById == null)
+            {
+                return NotFound();
+            }
+            _context.JobPostings.Remove(getJobpostingById);
+            _context.SaveChanges();
+
+            return Ok("Objekt wurde gelöscht");
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(JobPostingModel jobPostingModel)
+        {
+            if (jobPostingModel.Id == 0)    
+            {
+                return BadRequest("Objekt hat keine ID");
+            }
+            _context.JobPostings.Update(jobPostingModel);
+            _context.SaveChanges();
+            return Ok("Objekt erfolgreich GeUpdatet");
         }
     }
 }
